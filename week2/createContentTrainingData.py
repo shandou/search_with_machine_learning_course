ef334ce17fc8ee60e4ec7b3e5ae84b45670e1b6a
Shan Dou <shandou.seismo@gmail.com>
2022-07-02 06:37:28 +0000

    Implement category filtering logic

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -1,15 +1,22 @@
-from typing import List
-import re
 import argparse
-import multiprocessing
 import glob
+import itertools
+import multiprocessing
 import os
 import random
+import re
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from typing import Generator, List, Tuple
 
+import pandas as pd
 from nltk.stem import SnowballStemmer
 from tqdm import tqdm
+
+# Contants
+COLNAME_CATEGORY: str = "category_code"
+COLNAME_PROD_NAME: str = "normalized_product_name"
+COLNAME_N_PROD_PER_CATEGORY: str = "n_products_per_category"
 
 
 def transform_name(product_name: str) -> str:
@@ -76,12 +83,24 @@ general.add_argument(
     help="The rate at which to sample input (default is 1.0)",
 )
 
-# IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
+# IMPLEMENT: Setting min_products removes infrequent categories and
+# makes the classifier's task easier.
 general.add_argument(
     "--min_products",
     default=0,
     type=int,
     help="The minimum number of products per category (default is 0).",
+)
+
+# Configurable category pruning method
+#   - filter = simple exclusion of categories with n_products cutoff
+#   - rollup = merge child node with parent node in the taxonomy hierarchy
+general.add_argument(
+    "--pruning_method",
+    default="filter",
+    type=str,
+    choices=["filter", "rollup"],
+    help="Method used to prune category taxonomy (default to 'filter').",
 )
 
 args = parser.parse_args()
@@ -93,12 +112,61 @@ if os.path.isdir(output_dir) == False:
 
 if args.input:
     directory = args.input
-# IMPLEMENT:  Track the number of items in each category and only output if above the min
+# IMPLEMENT: Track the number of items in each category and
+#   only output if above the min
 min_products = args.min_products
 sample_rate = args.sample_rate
 names_as_labels = False
 if args.label == "name":
     names_as_labels = True
+pruning_method: str = args.pruning_method
+
+
+def filter_category_by_n_products(
+    df_all_labels: pd.DataFrame, min_products: int = 1
+) -> pd.DataFrame:
+    """Filter product category labels by ``min_products`` per category cutoff
+
+    Only categories with n_products > min_products are kept
+
+    Parameters
+    ----------
+    df_all_labels: pd.DataFrame
+        | Complete set of labelled data (category vs. product title)
+    min_products: int, default = 1
+        | n_products per category cutoff.
+        Categories with lower n_products are excluded in output dataframe
+
+    Returns
+    -------
+    pd.DataFrame
+        | labelled data after category filtering
+
+    """
+    # Obtain category list with n_products_per_category larger than
+    #   ``min_products`` cutoff
+    # The cutoff is to ensure each class label has sufficient
+    #   data coverage for ML classification
+    list_category_keeper: List[str] = list(
+        df_all_labels.groupby(COLNAME_CATEGORY)
+        .agg(
+            **{
+                COLNAME_N_PROD_PER_CATEGORY: pd.NamedAgg(
+                    column=COLNAME_PROD_NAME, aggfunc=lambda x: len(set(x))
+                )
+            }
+        )
+        .query(f"{COLNAME_N_PROD_PER_CATEGORY} >= {min_products}")
+        .index
+    )
+
+    # Apply category pruning
+    if len(list_category_keeper) > 0:
+        df_all_labels = df_all_labels.query(
+            f"{COLNAME_CATEGORY}.isin(@list_category_keeper)"
+        )
+
+    return df_all_labels
 
 
 def _label_filename(filename):
@@ -137,15 +205,35 @@ def _label_filename(filename):
 
 
 if __name__ == "__main__":
-    files = glob.glob(f"{directory}/*.xml")
+
+    files: List[str] = glob.glob(f"{directory}/*.xml")
 
     print("Writing results to %s" % output_file)
+
     with multiprocessing.Pool() as p:
-        all_labels = tqdm(
+
+        all_labels: Generator[List[List[Tuple[str, str]]], None, None] = tqdm(
             p.imap_unordered(_label_filename, files), total=len(files)
         )
 
-        with open(output_file, "w") as output:
-            for label_list in all_labels:
-                for (cat, name) in label_list:
-                    output.write(f"__label__{cat} {name}\n")
+        # Package category vs. product name data into pandas dataframe
+        #   ``itertools.chain.from_iterable(all_labels)`` is to flatten
+        #   nested iterable content list(list())
+        df_all_labels: pd.DataFrame = pd.DataFrame(
+            itertools.chain.from_iterable(all_labels),
+            columns=[COLNAME_CATEGORY, COLNAME_PROD_NAME],
+        )
+
+        # Prune taxonomy tree using selected method (filter or rollup)
+        if pruning_method == "filter":
+            df_all_labels = filter_category_by_n_products(
+                df_all_labels, min_products=min_products
+            )
+
+        # Add __label__ to category codes values
+        df_all_labels[COLNAME_CATEGORY] = df_all_labels[COLNAME_CATEGORY].map(
+            "__label__{}".format
+        )
+
+        # Write to ``output_file`` as tab-separated csv file (headerless)
+        df_all_labels.to_csv(output_file, index=False, header=False, sep="\t")
