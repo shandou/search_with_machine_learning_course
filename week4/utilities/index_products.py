b9584427752a2fb9142003eb1a9137d573b2ff89
Shan Dou <shandou.seismo@gmail.com>
2022-07-16 22:06:56 +0000

    import pretrained stransformer model

--- week4/utilities/index_products.py
+++ week4/utilities/index_products.py
@@ -19,6 +19,13 @@ logger.setLevel(logging.INFO)
 logging.basicConfig(format="%(levelname)s:%(message)s")
 
 # IMPLEMENT ME: import the sentence transformers module!
+import sentence_transformers
+from sentence_transformers import SentenceTransformer
+
+PRETRAINED_MODEL_NAME: str = "all-MiniLM-L6-v2"
+
+# model = SentenceTransformer(PRETRAINED_MODEL_NAME)
+# print(model)
 
 # NOTE: this is not a complete list of fields.  If you wish to add more, put in the appropriate XPath expression.
 # TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
@@ -161,7 +168,9 @@ def get_opensearch():
 def index_file(file, index_name, reduced=False):
     logger.info("Creating Model")
     # IMPLEMENT ME: instantiate the sentence transformer model!
-
+    model: sentence_transformers.SentenceTransformer = SentenceTransformer(
+        PRETRAINED_MODEL_NAME
+    )
     logger.info("Ready to index")
 
     docs_indexed = 0
